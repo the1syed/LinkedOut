d323d688a28234de7c3bda0ce0cdf703604c03b4
the1syed <05syed08@gmail.com>
2025-07-02 03:09:14 +0530

    Final

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,9 +5,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5000',
-    },
-  },
 });
